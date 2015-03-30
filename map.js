5ec72c2baf8940a56d7d08b556dffe58b3b52cc2
Heugue Xavier <xavier_heugue@ens.univ-artois.fr>
2015-03-30 17:42:00 +0200

    Maj map.js itineraire

--- map.js
+++ map.js
@@ -62,6 +62,70 @@ function initialisation () {
 	calcRoute(); 
  
 }
+
+function selectionner (choix) {
+    //var node = document.getElementById("liste");
+    var options;   // options est un tableau contenant les valeurs servant à l'auto-complétion
+    $( "#ville" ).autocomplete({
+        source: options
+    });
+    return options.length;
+
+}
+
+function trimChoix(mot){ //supprimer les " : "
+    var retour = mot.replace(":","");
+    return retour;
+}
+
+function createXhrObject(){
+    if (window.XMLHttpRequest)
+        return new XMLHttpRequest();
+
+    if (window.ActiveXObject){
+        var names = [ "Msxml2.XMLHTTP", "Microsoft.XMLHTTP",
+            "Msxml2.XMLHTTP.6.0", "Msxml2.XMLHTTP.3.0"];
+        for(var i in names){
+            try{ return new ActiveXObject(names[i]); }
+            catch(e){}
+        }
+    }
+    window.alert("pas de prise en charge de XMLHTTPRequest.");
+    return null; // non supporte
+}
+
+function recupListeVilles(codePostal) {
+    var req = createXhrObject();
+    if (null == req) return ;
+    req.onreadystatechange = function() {
+        // alert (req.readyState);
+        if(req.readyState == 4){
+            if(req.status == 200){
+                if (0==req.responseText.length) alert ('le code '+codePostal+" n'existe pas");
+                else {
+                    //alert('réponse du serveur :'+req.responseText);
+                    var tab = selectionner(req.responseText);
+                    if(tab == 2){
+                        document.getElementById("ville").value=trimChoix(req.responseText);
+                    }
+
+                }
+            } else {
+                alert("Error: returned status code " + req.status + " " + req.statusText);
+            }
+        }
+    };
+    req.open("POST", "assets/td3/td3_ex3_traitement.php", true);
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    req.send('code='+codePostal);
+} // submitForm
+
+function saisir () {
+    var texte = document.getElementById('code');
+    var nb=texte.value.length;
+    if (nb>4)recupListeVilles(texte.value);
+    else alert ('format code postal incorrect !')
+}
  
 function initEventHandlers(element, event, fx) {
     if (element.addEventListener)
@@ -70,4 +134,5 @@ function initEventHandlers(element, event, fx) {
         element.attachEvent('on' + event, fx);
 } // observe
  
+
 initEventHandlers(window, 'load', initialisation);
